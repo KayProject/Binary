8792a4d0d662d91054e8397abc958364349d4fbf
KayProject <jadonsunshine@gmail.com>
2026-07-27 06:23:07 +0100

    refactor(app): refactor layout, Globe

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { ReactNode } from "react";
 import { WalletProvider } from "@/components/WalletProvider";
 
+/**
+ * AppLayout
+ * @param {*} { children }: { children: ReactNode }
+ * @returns {*}
+ */
 export default function AppLayout({ children }: { children: ReactNode }) {
   return <WalletProvider>{children}</WalletProvider>;
 }
